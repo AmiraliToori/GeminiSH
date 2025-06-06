fd09f3dbb7711b45c530c5cf4ae53ebd4f481e27
AmiraliToori <master.masterblocker@gmail.com>
2025-06-06 09:52:04 +0330

    Add simple options to script
    Add simple options to script and handling them with "getopts"
    -l for listing the available models
    -m <name of the model> for choosing a specific models
    -p <prompt> for specify how to enter the prompt

--- GeminiSH.sh
+++ GeminiSH.sh
@@ -9,37 +9,71 @@ CYAN="\x1b[36m"
 RESET="\x1b[0m"
 
 #--------Configurations-----------#
-PROMPT="$1"
 MODEL="gemini-2.0-flash" # Default
 
-if [ -z "${1// /}" ]; then
-  echo "Usage: $0 \"<Your prompt goes here>\""
+#--------Models---------#
+list_models() {
+  curl -s "https://generativelanguage.googleapis.com/v1beta/models?key=$GEMINI_API_KEY" |
+    jq -r '.models[].name' 2>/dev/null |
+    awk -F/ '{print $2}' || printf "API Key not set or Invalid"
+  exit 0
+}
+
+#--------Main Function---------#
+main() {
+  printf "\n${BLUE}MODEL: ${CYAN}${BOLD}${MODEL^^}${RESET}\n"
+  JSON_PAYLOAD=$(jq -n \
+    --arg prompt_var "$prompt" \
+    '{
+      "contents": [
+        {
+          "parts": [
+            {
+              "text": $prompt_var
+            }
+          ]
+        }
+      ]
+    }')
+
+  curl "https://generativelanguage.googleapis.com/v1beta/models/"$MODEL":generateContent?key=$GEMINI_API_KEY" \
+    -s \
+    -H 'Content-Type: application/json' \
+    -X POST \
+    -d "$JSON_PAYLOAD" |
+    jq -r '.candidates[0].content.parts[0].text' | glow -
+}
+
+#---------Usage---------#
+usage() {
+  cat <<-EOF
+Usage: ./GeminiSH.sh <prompt> [options]
+
+A simple script to have a conversation with Gemini.
+
+OPTIONS:
+  -p, -p <Your prompt>             Set the prompt to send to Gemini.
+  -l,                              Print list of models.
+  -m, -m <Name of the model>       Set the model among the models available.
+EOF
   exit 1
-fi
+}
 
-if [ -n "$2" ] && [[ "${2,,}" =~ "^gemini" ]]; then
-  MODEL="$2"
-fi
+#----handling-options-----#
+while getopts ":p:m:l" opt; do
+  case "$opt" in
+  p) prompt="$OPTARG" ;;
+  l) list_models ;;
+  m) MODEL="$OPTARG" ;;
+  \?)
+    printf "Invalide option -$OPTARG\n" >&2
+    usage
+    ;;
+  :)
+    printf "Option -$OPTARG requires an argument." >&2
+    usage
+    ;;
+  esac
+done
 
-printf "\n${BLUE}MODEL: ${CYAN}${BOLD}${MODEL^^}${RESET}\n"
-
-JSON_PAYLOAD=$(jq -n \
-  --arg prompt_var "$PROMPT" \
-  '{
-    "contents": [
-      {
-        "parts": [
-          {
-            "text": $prompt_var
-          }
-        ]
-      }
-    ]
-  }')
-
-curl "https://generativelanguage.googleapis.com/v1beta/models/"$MODEL":generateContent?key=$GEMINI_API_KEY" \
-  -s \
-  -H 'Content-Type: application/json' \
-  -X POST \
-  -d "$JSON_PAYLOAD" |
-  jq -r '.candidates[0].content.parts[0].text' | glow -
+main
