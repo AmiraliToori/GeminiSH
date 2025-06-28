297d0cda2923ad7aa2a1e310827b43b210794104
AmiraliToori <master.masterblocker@gmail.com>
2025-06-28 18:19:32 +0330

    Add history ability to the GeminiSH

--- GeminiSH.sh
+++ GeminiSH.sh
@@ -56,12 +56,20 @@ main() {
       ]
     }')
 
-  curl "https://generativelanguage.googleapis.com/v1beta/models/"$model":generateContent?key=$GEMINI_API_KEY" \
+  local result=$(curl "https://generativelanguage.googleapis.com/v1beta/models/"$model":generateContent?key=$GEMINI_API_KEY" \
     -s \
     -H 'Content-Type: application/json' \
     -X POST \
     -d "$JSON_PAYLOAD" |
-    jq -r '.candidates[0].content.parts[0].text' | glow -
+    jq -r '.candidates[0].content.parts[0].text')
+
+  if [[ -n "$result" ]] || [[ "$result" != "null" ]]; then
+    local today_date="$(date "+%Y-%m-%d")"
+    local today_time="$(date "+%H:%M:%S")"
+    mkdir -p "./history/$today_date"
+
+    printf "$result" | tee "./history/$today_date/$today_time.md" | glow -
+  fi
 }
 
 source ./lib/ui.sh
