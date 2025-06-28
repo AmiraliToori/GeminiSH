6809e5dcd068bac302b05dbb157f7c2109f184d4
AmiraliToori <master.masterblocker@gmail.com>
2025-06-28 15:10:11 +0330

    Remove optget and add gum UI
    Remove and separate the method which responsible for getting a list of
    available models.
    Rename "MODEL" variable to "model"
    Source to ./lib/ui.sh because we want the changed variables in it.

--- GeminiSH.sh
+++ GeminiSH.sh
@@ -9,19 +9,11 @@ CYAN="\x1b[36m"
 RESET="\x1b[0m"
 
 #--------Configurations-----------#
-MODEL="gemini-2.5-flash" # Default
-
-#--------Models---------#
-list_models() {
-  curl -s "https://generativelanguage.googleapis.com/v1beta/models?key=$GEMINI_API_KEY" |
-    jq -r '.models[].name' 2>/dev/null |
-    awk -F/ '{print $2}' || printf "API Key not set or Invalid"
-  exit 0
-}
+model="gemini-2.5-flash" # Default
 
 #--------Main Function---------#
 main() {
-  printf "\n${BLUE}MODEL: ${CYAN}${BOLD}${MODEL^^}${RESET}\n"
+  printf "\n${BLUE}MODEL: ${CYAN}${BOLD}${model^^}${RESET}\n"
   JSON_PAYLOAD=$(jq -n \
     --arg prompt_var "$prompt" \
     '{
@@ -36,7 +28,7 @@ main() {
       ]
     }')
 
-  curl "https://generativelanguage.googleapis.com/v1beta/models/"$MODEL":generateContent?key=$GEMINI_API_KEY" \
+  curl "https://generativelanguage.googleapis.com/v1beta/models/"$model":generateContent?key=$GEMINI_API_KEY" \
     -s \
     -H 'Content-Type: application/json' \
     -X POST \
@@ -44,36 +36,5 @@ main() {
     jq -r '.candidates[0].content.parts[0].text' | glow -
 }
 
-#---------Usage---------#
-usage() {
-  cat <<-EOF
-Usage: ./GeminiSH.sh <prompt> [options]
-
-A simple script to have a conversation with Gemini.
-
-OPTIONS:
-  -p, -p <Your prompt>             Set the prompt to send to Gemini.
-  -l,                              Print list of models.
-  -m, -m <Name of the model>       Set the model among the models available.
-EOF
-  exit 1
-}
-
-#----handling-options-----#
-while getopts ":pm:l" opt; do
-  case "$opt" in
-  p) prompt=$(gum write --placeholder "Your Prompt") ;;
-  l) list_models ;;
-  m) MODEL="$OPTARG" ;;
-  \?)
-    printf "Invalide option -$OPTARG\n" >&2
-    usage
-    ;;
-  :)
-    printf "Option -$OPTARG requires an argument." >&2
-    usage
-    ;;
-  esac
-done
-
+source ./lib/ui.sh
 main
