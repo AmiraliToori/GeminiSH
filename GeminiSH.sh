a3ea6b5e421b73728f41abc4a8e5b4e01250c895
AmiraliToori <master.masterblocker@gmail.com>
2025-06-29 19:20:34 +0330

    Fixing slight bug and move the gradient_text_rgb method to lib/ as a separate function
    Fix the bug which leads to creating empty file in history when model
    refuse to generate answer (Poor Internet connection)

--- GeminiSH.sh
+++ GeminiSH.sh
@@ -3,45 +3,9 @@
 #--------Configurations-----------#
 model="gemini-2.5-flash" # Default
 
-function gradient_text_rgb {
-  local text="$1"
-  local start_r=$2
-  local start_g=$3
-  local start_b=$4
-  local end_r=$5
-  local end_g=$6
-  local end_b=$7
-  local segments=$((${#text}))
-  local i
-  local r_increment
-  local g_increment
-  local b_increment
-  local current_r
-  local current_g
-  local current_b
-
-  r_increment=$(((end_r - start_r) / segments))
-  g_increment=$(((end_g - start_g) / segments))
-  b_increment=$(((end_b - start_b) / segments))
-
-  current_r=$start_r
-  current_g=$start_g
-  current_b=$start_b
-
-  for ((i = 0; i < segments; i++)); do
-    printf "\e[38;2;${current_r};${current_g};${current_b}m${text:$i:1}"
-    current_r=$((current_r + r_increment))
-    current_g=$((current_g + g_increment))
-    current_b=$((current_b + b_increment))
-    # Clamp to the 0-255 range if needed
-  done
-
-  printf "\e[0m"
-}
-
 #--------Main Function---------#
 main() {
-  gradient_text_rgb "MODEL: $model" 255 0 0 0 0 255
+  ./lib/gradient_color.sh "MODEL: $model" 255 0 0 0 0 255
   JSON_PAYLOAD=$(jq -n \
     --arg prompt_var "$prompt" \
     '{
@@ -63,7 +27,7 @@ main() {
     -d "$JSON_PAYLOAD" |
     jq -r '.candidates[0].content.parts[0].text')
 
-  if [[ -n "$result" ]] || [[ "$result" != "null" ]]; then
+  if [[ -n "$result" ]]; then
     local today_date="$(date "+%Y-%m-%d")"
     local today_time="$(date "+%H:%M:%S")"
     mkdir -p "./history/$today_date"
