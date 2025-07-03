a96b7b18214b2fb32c3dd78c9f8e2da3c233f6b8
AmiraliToori <master.masterblocker@gmail.com>
2025-07-03 16:27:59 +0330

    Slight fixes and remove the color changes option

--- lib/ui.sh
+++ lib/ui.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-function change_colors_menu() {
-  local selected_color=$(gum choose "Default" "Gum")
-}
-
 function intro() {
   clear
   gum style \
@@ -19,28 +15,41 @@ function intro() {
     "Current model: $model"
 }
 
-function error_spinner() {
+function error_page() {
   local error_text="$1"
+  local loading_text="$2"
+  clear
+  gum style \
+    --background "#070708" \
+    --border hidden \
+    --bold \
+    --border-foreground 212 \
+    --width 50 \
+    --margin "1 20" \
+    --padding "1 2" \
+    --align center \
+    --foreground "#E40406" \
+    " $1 "
   gum spin --spinner="dot" \
-    --title.foreground="#FFFF00" \
-    --spinner.foreground="#E7E7E7" \
-    --align="left" \
-    --title="$1" \
-    -- sleep 3
+    --align="center" \
+    --title "$1" \
+    -- sleep 1
+  clear
 }
 
 function prompt_box() {
   clear
   local prompt="$1"
   gum style \
+    --foreground "$FOREGROUND_COLOR" \
     --align left \
-    "$(gum style --foreground="116" --bold --underline "PROMPT:") $prompt"
+    "$(gum style --foreground 212 --bold --underline "PROMPT:") $prompt"
 }
 
 function take_prompt_menu() {
   prompt=$(gum write --height 15 --placeholder="Write your prompt")
   if [[ -z "${prompt// /}" ]]; then
-    error_spinner "Prompt cannot be empty. Please try again."
+    error_page "Prompt cannot be empty. Please try again." "Loading"
     take_prompt_menu
   fi
   prompt_box "$prompt"
@@ -48,18 +57,9 @@ function take_prompt_menu() {
 
 function choose_model_menu() {
   clear
-  local list_of_models=$(./lib/models_list.sh)
-
-  if [[ -z "$list_of_models" ]]; then
-    error_spinner "No model selected. Please try again."
-    ./GeminiSH.sh
-  fi
-
-  model=$(gum choose $list_of_models)
-
+  model=$(gum choose $(./lib/models_list.sh))
   if [[ -z $model ]]; then
-    clear
-    error_spinner "No model selected. Please try again."
+    error_page "No model selected. Please try again." "Loading"
     ./GeminiSH.sh
   else
     clear
@@ -79,13 +79,12 @@ function exit_menu() {
 }
 
 function options() {
-  local option=$(gum choose --header "The Menu:" "Prompt" "Choose a model" "History" "Change Colors" "Exit")
+  local option=$(gum choose --header "The Menu:" "Prompt" "Choose a model" "History" "Exit")
 
   case "$option" in
   "Prompt") take_prompt_menu ;;
   "Choose a model") choose_model_menu ;;
   "History") history_menu ;;
-  "Change Colors") change_colors_menu ;;
   "Exit") exit_menu ;;
   *)
     clear
