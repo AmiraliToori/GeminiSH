6973b3f9e144a9bb7b74a317465ceccf0cdae7e5
AmiraliToori <master.masterblocker@gmail.com>
2025-07-05 12:07:13 +0330

    Delete and move these script and create a helper method from them

--- lib/models_list.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-curl -s "https://generativelanguage.googleapis.com/v1beta/models?key=$GEMINI_API_KEY" |
-  jq -r '.models[].name' 2>/dev/null |
-  awk -F/ '{print $2}' || printf "API Key not set or Invalid" && exit 1
