3d7633b4fd74cb13de9ea715cc979d2704c35e9c
AmiraliToori <master.masterblocker@gmail.com>
2025-07-05 17:27:01 +0330

    Add spinner for fetching the list of models
    Now there is a spinner when fetching the list of models.

--- lib/choose_model_menu.sh
+++ lib/choose_model_menu.sh
@@ -4,11 +4,22 @@ source ./lib/utils.sh
 current_model="$1"
 
 clear >&2
-selected_model=$(gum choose $(generate_models_list))
+export -f generate_models_list
+export GEMINI_API_KEY
+
+selected_model=$(gum choose $(gum spin \
+  --spinner="line" \
+  --title="Fetching..." \
+  -- bash -c 'generate_models_list'))
 if [[ -z $selected_model ]] || [[ $selected_model == "nothing selected" ]]; then
-  error_page "No model selected. Please try again." "Loading"
+  error_page "No model selected. Please try again."
   printf "$current_model"
 else
+  gum style --foreground="33" \
+    --underline \
+    --padding "1 1" \
+    --align "center" \
+    "You have been selected: $selected_model" >&2 && sleep 2
   printf "$selected_model"
-  gum style "You have been selected: $selected_model" >&2
+
 fi
