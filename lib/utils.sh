71597165e85266e098a57ddf1448b45cce29ec45
AmiraliToori <master.masterblocker@gmail.com>
2025-07-05 17:24:27 +0330

    Changes in error_page
    Remove the "gum style" and instead just shows a spinner for errors.

--- lib/utils.sh
+++ lib/utils.sh
@@ -2,23 +2,12 @@
 
 function error_page {
   local error_text="$1"
-  local loading_text="$2"
   clear >&2
-  gum style \
-    --background "#070708" \
-    --border hidden \
-    --bold \
-    --border-foreground 212 \
-    --width 50 \
-    --margin "1 20" \
-    --padding "1 2" \
-    --align center \
-    --foreground "#E40406" \
-    " $error_text " >&2
-
   gum spin --spinner="dot" \
     --align="left" \
-    --title "$loading_text" \
+    --title.foreground="#E40406" \
+    --spinner.foreground="#E40406" \
+    --title "$error_text" \
     -- sleep 3 >&2
   clear >&2
 }
