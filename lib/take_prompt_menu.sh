f87208c349835ca41ee98767af450a3baa22227c
AmiraliToori <master.masterblocker@gmail.com>
2025-07-05 15:33:00 +0330

    Enhance thinking spinner
    Now the thinking spinner for generating answer is animating until the
    final answer generates.

--- lib/take_prompt_menu.sh
+++ lib/take_prompt_menu.sh
@@ -4,23 +4,10 @@ source ./lib/utils.sh
 
 model="$1"
 
-function prompt_box() {
-  clear >&2
-  local prompt="$1"
-  gum style \
-    --align left \
-    "$(gum style --foreground 212 --bold --underline "PROMPT:") $prompt" >&2
-}
-
-prompt=$(gum write --height 15 --show-line-numbers --placeholder="Write your prompt")
-if [[ -z "${prompt// /}" ]]; then
-  error_page "Prompt cannot be empty. Please try again." "Loading"
-fi
-prompt_box "$prompt"
-
-JSON_PAYLOAD=$(jq -n \
-  --arg prompt_var "$prompt" \
-  '{
+function main {
+  local JSON_PAYLOAD=$(jq -n \
+    --arg prompt_var "$prompt" \
+    '{
     "contents": [
       {
         "parts": [
@@ -32,20 +19,37 @@ JSON_PAYLOAD=$(jq -n \
     ]
   }')
 
-result=$(curl "https://generativelanguage.googleapis.com/v1beta/models/"$model":generateContent?key=$GEMINI_API_KEY" \
-  -s \
-  -H 'Content-Type: application/json' \
-  -X POST \
-  -d "$JSON_PAYLOAD" |
-  jq -r '.candidates[0].content.parts[0].text')
+  local result=$(curl "https://generativelanguage.googleapis.com/v1beta/models/"$model":generateContent?key=$GEMINI_API_KEY" \
+    -s \
+    -H 'Content-Type: application/json' \
+    -X POST \
+    -d "$JSON_PAYLOAD" |
+    jq -r '.candidates[0].content.parts[0].text')
 
-if [[ -n "$result" ]]; then
-  today_date="$(date "+%Y-%m-%d")"
-  today_time="$(date "+%H:%M:%S")"
-  mkdir -p "./history/$today_date"
+  printf "$result"
+}
 
-  printf "$result" | tee "./history/$today_date/$today_time.md" | glow - >&2
+prompt=$(gum write --height 15 --show-line-numbers --placeholder="Write your prompt")
+if [[ -z "${prompt// /}" ]]; then
+  error_page "Prompt cannot be empty. Please try again." "Loading"
   printf "0"
 else
-  error_page " There is something wrong, Please check the connection." "Loading"
+
+  export -f main
+  export prompt
+  export model
+  export GEMINI_API_KEY
+
+  result=$(gum spin --spinner="dot" --title="Thinking..." -- bash -c 'main')
+  clear >&2
+  if [[ -n "$result" ]]; then
+    today_date="$(date "+%Y-%m-%d")"
+    today_time="$(date "+%H:%M:%S")"
+    mkdir -p "./history/$today_date"
+
+    prompt_box "$prompt"
+    printf "$result" | tee "./history/$today_date/$today_time.md" | glow - >&2
+  else
+    error_page " There is something wrong, Please check the connection." "Loading"
+  fi
 fi
